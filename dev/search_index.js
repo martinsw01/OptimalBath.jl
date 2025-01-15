3cbf05b701ee2c538669b4b3bde5fbc93cc28ae0
Documenter.jl <documenter@juliadocs.github.io>
2025-01-15 15:41:10 +0000

    build based on 1d50228

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OptimalBath","category":"page"},{"location":"#OptimalBath","page":"Home","title":"OptimalBath","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OptimalBath.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OptimalBath]","category":"page"}]
+}
