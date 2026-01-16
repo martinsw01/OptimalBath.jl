d9543ae36b7c59b52e868fabfbc89b24a7d2bc62
Documenter.jl <documenter@juliadocs.github.io>
2026-01-16 09:54:34 +0000

    build based on eb3d656

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OptimalBath","category":"page"},{"location":"#OptimalBath","page":"Home","title":"OptimalBath","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OptimalBath.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OptimalBath]","category":"page"}]
+[{"location":"#OptimalBath","page":"Home","title":"OptimalBath","text":"Documentation for OptimalBath.\n\n","category":"section"},{"location":"#OptimalBath.compute_flux_source-NTuple{4, Any}","page":"Home","title":"OptimalBath.compute_flux_source","text":"compute_flux_source(Λ, Ul, Ur, Δx)\n\nComputes the source due to the reformulation of the adjoint pde to conservative form.\n\n\n\n\n\n","category":"method"}]
 }
