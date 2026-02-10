eec3d29b65447b29a46f70e4af7c01baa65b97db
martinsw01 <martin.s.winther@gmail.com>
2026-02-10 15:54:00 +0100

    Attempt to handle dry fronts in adjoint equations

--- src/AdjointSWE.jl
+++ src/AdjointSWE.jl
@@ -2,8 +2,21 @@ export solve_adjoint
 
 using StaticArrays
 
+const desingularizing_kappa = 1e-5
+
+function desingularize(h, p)
+    h_star = desingularize(h)
+    return p / h_star
+end
+
+function desingularize(h)
+    h_star = copysign(1, h)*max(abs(h), min(h^2/(2*desingularizing_kappa) + desingularizing_kappa/2.0, desingularizing_kappa))
+    return h_star
+end
+
 function swe_jacobian_transpose(h, hu)
-    return @SMatrix [0.0 9.81*h - (hu/h)^2; 1.0 2*(hu/h)]
+    u = desingularize(h, hu)
+    return @SMatrix [0.0 9.81*h - u^2; 1.0 2*u]
 end
 
 function flux(Λ, U)
@@ -12,7 +25,7 @@ function flux(Λ, U)
 end
 
 function compute_eigenvalues(h, hu)
-    u = hu / h
+    u = desingularize(h, hu)
     c = sqrt(9.81 * h)
     return u + c, u - c
 end
@@ -72,7 +85,7 @@ function compute_next_Λ_right_boundary(Λl, Λc, Ul, Uc, dJdU, bl, br, Δt, Δx
     return compute_next_Λ(Λl, Λc, Λr, Ul, Uc, Ur, dJdU, bl, br, Δt, Δx)
 end
 
-function compute_semi_discrete_derivative(Λl, Λc, Λr, Ul, Uc, Ur, dJdU, bl, br, Δt, Δx)
+function compute_semi_discrete_derivative(Λl, Λc, Λr, Ul, Uc, Ur, dJdU, bl, br, Δt, Δx)        
     Fl = numerical_flux(Λl, Λc, Ul, Uc)
     Fr = numerical_flux(Λc, Λr, Uc, Ur)
     dfₓᵀΛ = compute_flux_source(Λc, Ul, Ur, Δx)
@@ -84,8 +97,9 @@ function compute_next_Λ(Λl, Λc, Λr, Ul, Uc, Ur, dJdU, bl, br, Δt, Δx)
     Λc + Δt * compute_semi_discrete_derivative(Λl, Λc, Λr, Ul, Uc, Ur, dJdU, bl, br, Δt, Δx)
 end
 
+const AverageDepthStates{T, N, A} = States{Average, Depth, T, N, A}
 
-function solve_adjoint(Λ0, U::States{Average, Depth, T, D, A}, dJdU, b, t, Δx) where {T, D, A}
+function solve_adjoint(Λ0, U::AverageDepthStates, dJdU, b, t, Δx)
     U = U.U
     Λ = similar(U)
     N, M = size(U)
@@ -112,4 +126,128 @@ function solve_adjoint(Λ0, U::States{Average, Depth, T, D, A}, dJdU, b, t, Δx)
                                                   Δt, Δx)
     end
     return Λ
+end
+
+const LeftDepthStates{T, N, A} = States{Left, Depth, T, N, A}
+const RightDepthStates{T, N, A} = States{Right, Depth, T, N, A}
+
+wet(Ul⁺, Uc⁻, Uc⁺, Ur⁻) = !left_interface_dry(Ul⁺, Uc⁻) && !right_interface_dry(Uc⁺, Ur⁻)
+right_interface_dry(Uc⁺, Ur⁻) = height(Uc⁺) < desingularizing_kappa || height(Ur⁻) < desingularizing_kappa
+left_interface_dry(Ul⁺, Uc⁻) = height(Ul⁺) < desingularizing_kappa || height(Uc⁻) < desingularizing_kappa
+only_left_dry(Ul⁺, Uc⁻, Uc⁺, Ur⁻) = left_interface_dry(Ul⁺, Uc⁻) && !right_interface_dry(Uc⁺, Ur⁻)
+only_right_dry(Ul⁺, Uc⁻, Uc⁺, Ur⁻) = right_interface_dry(Uc⁺, Ur⁻) && !left_interface_dry(Ul⁺, Uc⁻)
+
+
+function solve_adjoint(Λ0, Ul::LeftDepthStates, Ur::RightDepthStates, dJdU, b, t, Δx)
+    Ul = Ul.U
+    Ur = Ur.U
+    Λ = similar(Ul)
+
+    N, M = size(Ul)
+    Λ[:, end] .= Λ0
+    for n in M:-1:2
+        Δt = t[n] - t[n-1]
+        Λ[1, n-1] = compute_next_Λ_left_boundary(Λ[1:2, n]...,
+                                                 Ul[1:2, n]...,
+                                                 Ur[1, n],
+                                                 dJdU[1, n],
+                                                 b[1:2]...,
+                                                 Δt, Δx)
+        for i in 2:N-1
+            stencil = Ur[i-1, n], Ul[i, n], Ur[i, n], Ul[i+1, n]
+            if wet(stencil...)
+                 Λ[i, n-1] = compute_next_Λ(Λ[i-1:i+1, n]...,
+                                           Ul[i:i+1, n]...,
+                                           Ur[i-1:i, n]...,
+                                           dJdU[i, n],
+                                           b[i:i+1]...,
+                                           Δt, Δx)
+            elseif only_left_dry(stencil...)
+                Λ[i, n-1] = compute_next_Λ_left_boundary(Λ[i:i+1, n]...,
+                                                        Ul[i:i+1, n]...,
+                                                        Ur[i, n],
+                                                        dJdU[i, n],
+                                                        b[i:i+1]...,
+                                                        Δt, Δx)
+            elseif only_right_dry(stencil...)
+                Λ[i, n-1] = compute_next_Λ_right_boundary(Λ[i-1:i, n]...,
+                                                        Ul[i, n],
+                                                        Ur[i-1:i, n]...,
+                                                        dJdU[i, n],
+                                                        b[i:i+1]...,
+                                                        Δt, Δx)
+            else
+                Λ[i, n-1] = Λ[i, n]
+            end
+        end
+        Λ[N, n-1] = compute_next_Λ_right_boundary(Λ[N-1:N, n]...,
+                                                  Ul[N, n],
+                                                  Ur[N-1:N, n]...,
+                                                  dJdU[N, n],
+                                                  b[N:N+1]...,
+                                                  Δt, Δx)
+    end
+    return Λ
+end
+
+function compute_next_Λ(Λl, Λc, Λr, Uc⁻, Ur⁻, Ul⁺, Uc⁺, dJdU, bl, br, Δt, Δx)
+    return Λc + Δt * compute_semi_discrete_derivative(Λl, Λc, Λr, Uc⁻, Ur⁻, Ul⁺, Uc⁺, dJdU, bl, br, Δt, Δx)
+end
+
+
+function compute_next_Λ_left_boundary(Λc, Λr, Uc⁻, Ur⁻, Uc⁺, dJdU, bl, br, Δt, Δx)
+    Λl = compute_adjoint_ghost_cell(Λc, Λr, Uc⁻)
+    # Λl = compute_ghost_cell(Λc, nothing) #compute_adjoint_ghost_cell(Λc, Λr, Uc⁻)
+    Ul⁺ = compute_ghost_cell(Uc⁻, nothing)
+    return compute_next_Λ(Λl, Λc, Λr, Uc⁻, Ur⁻, Ul⁺, Uc⁺, dJdU, bl, br, Δt, Δx)
+end
+
+function compute_next_Λ_right_boundary(Λl, Λc, Uc⁻, Ul⁺, Uc⁺, dJdU, bl, br, Δt, Δx)
+    Λr = compute_adjoint_ghost_cell(Λc, Λl, Uc⁺)
+    # Λr = compute_ghost_cell(Λc, nothing) #compute_ghost_cell(Λc, nothing)
+    Ur⁻ = compute_ghost_cell(Uc⁺, nothing)
+    return compute_next_Λ(Λl, Λc, Λr, Uc⁻, Ur⁻, Ul⁺, Uc⁺, dJdU, bl, br, Δt, Δx)
+end
+
+function compute_semi_discrete_derivative(Λl, Λc, Λr, Uc⁻, Ur⁻, Ul⁺, Uc⁺, dJdU, bl, br, Δt, Δx)        
+    Fl = numerical_flux(Λl, Λc, Ul⁺, Uc⁻)
+    Fr = numerical_flux(Λc, Λr, Uc⁺, Ur⁻)
+    dfₓᵀΛ = compute_flux_source_improved(Λc, Uc⁻, Uc⁺, Δx)
+    SᵀΛ = compute_bathymetry_source_improved(Λc, Uc⁻, Uc⁺, bl, br, Δx)
+    return - (Fr - Fl)/Δx - dfₓᵀΛ + SᵀΛ + dJdU
+end
+
+function compute_S2(Ul, Ur, bl, br, Δx)
+    hl = height(Ul)
+    hr = height(Ur)
+
+    if hl < desingularizing_kappa && hr > desingularizing_kappa
+        return S2 = 9.81 * hr / Δx
+    elseif hr < desingularizing_kappa && hl > desingularizing_kappa
+        return S2 = - 9.81 * hl / Δx
+    else
+        return S2 = - 9.81 * (br - bl) / Δx
+    end
+end
+
+function compute_bathymetry_source_improved(Λc, Ul, Ur, bl, br, Δx)
+    λ2 = Λc[2]
+    S2 = compute_S2(Ul, Ur, bl, br, Δx)
+    return State(S2 * λ2, 0.0)
+end
+
+function compute_flux_source_improved(Λ, Ul, Ur, Δx)
+    hl, pl = Ul
+    hr, pr = Ur
+
+    # Regularization:
+    if hl < Δx
+        pl *= hl
+    end
+    if hr < Δx
+        pr *= hr
+    end
+        
+    dfₓᵀ = (swe_jacobian_transpose(hr, pr) - swe_jacobian_transpose(hl, pl)) / Δx
+    return dfₓᵀ * Λ
 end
