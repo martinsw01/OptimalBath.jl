0cd5d5449d141da042164e29efcc57ce71ae0356
martinsw01 <martin.s.winther@gmail.com>
2026-02-10 09:44:01 +0100

    - Created linear reconstruction that is well balanced near dry fronts - Corresponding bottom source term

--- src/PrimalSWE.jl
+++ src/PrimalSWE.jl
@@ -91,5 +91,92 @@ function compute_Δx(problem::SinFVMPrimalSWEProblem)
 end
 
 function initial_state(problem::SinFVMPrimalSWEProblem)
-    return States{Average, Elevation}(problem.u0)
+    return problem.u0
+end
+
+using SinFVM: Reconstruction, for_each_cell, B_cell, B_face_left, B_face_right, minmod_slope, AllPracticalSWE, for_each_inner_cell
+import SinFVM: reconstruct!
+
+"""
+    BathymetryHandlingReconstruction()
+A linear reconstruction similar to SinFVM.LinearReconstruction, but caps heights and momentum to zero instead of adjusting the slope.
+"""
+struct BathymetryHandlingReconstruction <: Reconstruction end
+
+function clip_to_zero(U, b)
+    h = height(U)
+    if h > b
+        return State(h - b, momentum(U))
+    else
+        return zero(U)
+    end
+end
+
+function reconstruct_cell(U, B_left, B_right)
+    slope = minmod_slope(U, U, U, 1)
+    U_left = U .- 0.5 .* slope
+    U_right = U .+ 0.5 .* slope
+    return clip_to_zero(U_left, B_left), clip_to_zero(U_right, B_right)
+end
+
+function SinFVM.reconstruct!(backend, ::BathymetryHandlingReconstruction, output_left, output_right, input_conserved, grid::SinFVM.Grid, eq::AllPracticalSWE, direction)
+    @assert grid.ghostcells[1] > 1
+
+    for_each_inner_cell(backend, grid, direction; ghostcells=1) do ileft, imiddle, iright
+
+        B_left = B_face_left(eq.B, imiddle, direction)
+        B_right = B_face_right(eq.B, imiddle, direction)
+        U_left, U_right = reconstruct_cell(input_conserved[imiddle], B_left, B_right)
+
+        output_left[imiddle], output_right[imiddle] = U_left, U_right
+    end
+
+    return nothing
+end
+
+using SinFVM: SourceTerm, ConservedSystem, Direction, compute_dx
+import SinFVM: evaluate_directional_source_term!
+
+
+"""
+    DryFrontTerm()
+
+A bottom topography source term corresponding to `BathymetryHandlingReconstruction`. Similar to the one used in SinFVM,
+except that it balances the fluxes also at dry fronts, where the water height goes to zero.
+"""
+struct DryFrontTerm <: SourceTerm end
+
+function SinFVM.evaluate_directional_source_term!(::DryFrontTerm, output, current_state, cs::ConservedSystem, dir::Direction)
+    # {right, left}_buffer is (h, hu)
+    # output and current_state is (w, hu)
+    dx = compute_dx(cs.grid, dir)
+    output_momentum = output.hu
+    B = cs.equation.B 
+    g = cs.equation.g
+    h_right = cs.right_buffer.h
+    h_left  = cs.left_buffer.h
+    for_each_inner_cell(cs.backend, cs.grid, dir) do ileft, imiddle, iright
+        B_right = B_face_right( B, imiddle, dir)
+        B_left  = B_face_left(B, imiddle, dir)
+
+        output_momentum[imiddle] += compute_source_term(h_left[imiddle], h_right[imiddle], B_left, B_right, g, dx, cs.equation.depth_cutoff)
+    end
+end
+
+function compute_source_term(hl, hr, Bl, Br, g, dx, depth_cutoff)
+    if only_right_dry(hl, hr, depth_cutoff)
+        return -0.5 * g * hl^2 / dx
+    elseif only_left_dry(hl, hr, depth_cutoff)
+        return 0.5 * g * hr^2 / dx
+    else
+        return - g*((Br - Bl)/dx)*((hr + hl)/2.0)
+    end
+end
+
+function only_left_dry(h_left, h_right, depth_cutoff)
+    return h_left < depth_cutoff && h_right > depth_cutoff
+end
+
+function only_right_dry(h_left, h_right, depth_cutoff)
+    return h_left > depth_cutoff && h_right < depth_cutoff
 end
